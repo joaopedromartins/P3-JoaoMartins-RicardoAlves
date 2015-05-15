c0742af3433dc80ceb140fc3e387a5e2fe06daa2
Joao Martins <joao.pedro.brandao.martins@gmail.com>
2015-05-15 17:25:46 +0100

    calc
    chat com painel com scroll

--- P3-JoaoMartins-RicardoAlves/src/main/webapp/resources/js/chat.js
+++ P3-JoaoMartins-RicardoAlves/src/main/webapp/resources/js/chat.js
@@ -4,11 +4,11 @@ window.onload =function() {
 		var btn = $("#chatform\\:btnhidden");
 		
 		btn[0].click();
-	},1000);
+	},2000);
 	
 	setInterval(function(){
 		var btn2 = $("#estatisticaform\\:btnhidden2");
 		
 		btn2[0].click();
-	},1000);
+	},2000);
 }
